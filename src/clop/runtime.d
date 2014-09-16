91c10c79acf5c10e806d14eabc380e5e05f31e60
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2014-09-16 10:34:40 +0200

    Add verbose runtime option. Improve assertions.

--- src/clop/runtime.d
+++ src/clop/runtime.d
@@ -13,38 +13,37 @@ struct Runtime
 
   /**
    */
-  void init( uint platform_id = 0, uint device_id = 0 )
+  void init( uint platform_id = 0, uint device_id = 0, bool verbose = true )
   {
     DerelictCL.load();
     cl_uint num_platforms, num_devices;
-    status = clGetPlatformIDs( 0, null, &num_platforms );
-    assert( status == CL_SUCCESS && num_platforms > 0 && platform_id < num_platforms, "No OpenCL platform found." );
-    auto platforms = new cl_platform_id[num_platforms];
-    assert( platforms != null, "Can't allocate array of OpenCL platform IDs." );
-    status = clGetPlatformIDs( num_platforms, platforms.ptr, null );
-    assert( status == CL_SUCCESS, "Can't get OpenCL platform IDs." );
+    status = clGetPlatformIDs( 0, null, &num_platforms );                                    assert( status == CL_SUCCESS && num_platforms > 0 && platform_id < num_platforms, "No OpenCL platform found: " ~ cl_strerror( status ) );
+    auto platforms = new cl_platform_id[num_platforms];                                      assert( platforms != null, "Can't allocate array of OpenCL platform IDs." );
+    status = clGetPlatformIDs( num_platforms, platforms.ptr, null );                         assert( status == CL_SUCCESS, "Can't get OpenCL platform IDs: " ~ cl_strerror( status ) );
     auto platform = platforms[platform_id];
-    status = clGetDeviceIDs( platform, CL_DEVICE_TYPE_ALL, 0, null, &num_devices );
-    assert( status == CL_SUCCESS && num_devices > 0 && device_id < num_devices, "No OpenCL device found." );
-    auto devices = new cl_device_id[num_devices];
-    assert( devices != null, "Can't allocate array of OpenCL device IDs." );
-    status = clGetDeviceIDs( platform, CL_DEVICE_TYPE_ALL, num_devices, devices.ptr, null );
-    assert( status == CL_SUCCESS, "Can't get OpenCL device IDs." );
+    status = clGetDeviceIDs( platform, CL_DEVICE_TYPE_ALL, 0, null, &num_devices );          assert( status == CL_SUCCESS && num_devices > 0 && device_id < num_devices, "No OpenCL device found:" ~ cl_strerror( status ) );
+    auto devices = new cl_device_id[num_devices];                                            assert( devices != null, "Can't allocate array of OpenCL device IDs." );
+    status = clGetDeviceIDs( platform, CL_DEVICE_TYPE_ALL, num_devices, devices.ptr, null ); assert( status == CL_SUCCESS, "Can't get OpenCL device IDs: " ~ cl_strerror( status ) );
     device = devices[device_id];
-    context = clCreateContext( null, 1, &device, null, null, &status );
-    assert( status == CL_SUCCESS, "Can't create OpenCL context." );
-    queue = clCreateCommandQueue( context, device, 0, &status );
-    assert( status == CL_SUCCESS, "Can't create OpenCL command queue." );
+    context = clCreateContext( null, 1, &device, null, null, &status );                      assert( status == CL_SUCCESS, "Can't create OpenCL context: " ~ cl_strerror( status ) );
+    queue = clCreateCommandQueue( context, device, 0, &status );                             assert( status == CL_SUCCESS, "Can't create OpenCL command queue:" ~ cl_strerror( status ) );
+    if ( verbose )
+    {
+      size_t value_size;
+      status = clGetDeviceInfo( device, CL_DEVICE_NAME, 0, null, &value_size );              assert( value_size > 0, "Can't get the device name: " ~ cl_strerror( status ) );
+      char[] buffer = new char[value_size];                                                  assert( buffer != null, "Can't allocate buffer to hold the device name." );
+      status = clGetDeviceInfo( device, CL_DEVICE_NAME, value_size, buffer.ptr, null );      assert( status == CL_SUCCESS, "Can't get the device name: " ~ cl_strerror( status ) );
+      if ( buffer[$] == '\0' ) buffer.length -= 1;
+      writefln( "OpenCL device: \"%s\"", buffer );
+    }
   }
 
   /**
    */
   void shutdown()
   {
-    status = clReleaseCommandQueue( queue );
-    assert( status == CL_SUCCESS, "clReleaseCommandQueue failed." );
-    status = clReleaseContext( context );
-    assert( status == CL_SUCCESS, "clReleaseContext failed." );
+    status = clReleaseCommandQueue( queue ); assert( status == CL_SUCCESS, "clReleaseCommandQueue failed." );
+    status = clReleaseContext( context );    assert( status == CL_SUCCESS, "clReleaseContext failed." );
     writeln( "CLOP runtime shut down." );
   }
 }
