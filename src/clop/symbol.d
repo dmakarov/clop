2bb0b7d031320b690e449f90b6145c34e5e55d03
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2015-03-24 17:11:59 +0100

    Formatting and comments.

--- src/clop/symbol.d
+++ src/clop/symbol.d
@@ -23,18 +23,19 @@ struct Symbol
   {
     uses = uses.dup;
     defs = defs.dup;
+    box = box.dup;
   }
 
   string toString()
   {
-    return format( "name %10s, type %10s, %6s, %6s, #uses %2d, #defs %2d, shadow %s",
-                   name,
-                   (type == null ? "N/A" : type),
-                   (uses == null ? 0 : uses.length),
-                   (defs == null ? 0 : defs.length),
-                   (is_local ? "local" : "global"),
-                   (is_array ? "array" : "scalar"),
-                   (can_cache ? "can" : "cannot" ) ~ " put in local memory" );
+    return format("name %10s, type %10s, %6s, %6s, #uses %2d, #defs %2d, shadow %s",
+                  name,
+                  (type == null ? "N/A" : type),
+                  (uses == null ? 0 : uses.length),
+                  (defs == null ? 0 : defs.length),
+                  (is_local ? "local" : "global"),
+                  (is_array ? "array" : "scalar"),
+                  (can_cache ? "can" : "cannot" ) ~ " put in local memory");
   }
 }
 
