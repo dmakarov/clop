a4cb2370b7db35ed560bb778bba44676f9d45270
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2015-03-27 19:35:02 +0100

    Refactor algebra example to include all OpenCL code in one function.

--- examples/algebra/src/app.d
+++ examples/algebra/src/app.d
@@ -37,9 +37,9 @@ import clop.compiler;
 /++
  +/
 class Application {
+
   static immutable int SEED  =  1;
 
-  cl_kernel kernel;
   float[] A, B, C;
   size_t size;
 
@@ -58,52 +58,18 @@ class Application {
 
     Mt19937 gen;
     gen.seed(SEED);
-    foreach (r; 0 .. size)
+    foreach (i; 0 .. size)
     {
-      A[r] = uniform(0f, 1f, gen);
-      B[r] = uniform(0f, 1f, gen);
+      A[i] = uniform(0f, 1f, gen);
+      B[i] = uniform(0f, 1f, gen);
     }
-
-    /++
-     +/
-    char[] code = q{
-      __kernel void vector_sum(__global float* a, __global float* b, __global float* c)
-      {
-        int tx = get_global_id(0);
-        c[tx] = a[tx] + b[tx];
-      } /* vector_sum */
-    }.dup;
-    cl_int status;
-    size_t size = code.length;
-    char*[] strs = [code.ptr];
-    auto program = clCreateProgramWithSource(runtime.context, 1, strs.ptr, &size, &status);
-    assert(status == CL_SUCCESS, "this " ~ cl_strerror(status));
-    status = clBuildProgram(program, 1, &runtime.device, "", null, null);
-
-    if (status != CL_SUCCESS)
-    {
-      char[3072] log;
-      clGetProgramBuildInfo(program, runtime.device, CL_PROGRAM_BUILD_LOG, 3071, log.ptr, null);
-      writeln("CL_PROGRAM_BUILD_LOG:\n", log, "\nEOD");
-    }
-
-    assert(status == CL_SUCCESS, "this " ~ cl_strerror(status));
-    kernel = clCreateKernel(program, "vector_sum", &status);
-    assert(status == CL_SUCCESS, "this " ~ cl_strerror(status));
-    status = clReleaseProgram(program);
-    assert(status == CL_SUCCESS, "this " ~ cl_strerror(status));
-  } // this()
+  } // this
 
   /++
    +/
-  void validate()
+  void clop_compute()
   {
-    uint diff = 0;
-    foreach (ii; 0 .. size)
-      if (C[ii] != A[ii] + B[ii])
-        ++diff;
-    if (diff > 0)
-      writeln("DIFFs ", diff);
+    mixin (compile(q{ NDRange(i : 0 .. size) { C[i] = A[i] + B[i]; } }));
   }
 
   /++
@@ -112,14 +78,40 @@ class Application {
   {
     try
     {
+      char[] code = q{
+        __kernel void vector_sum(__global float* a, __global float* b, __global float* c)
+        {
+          int tx = get_global_id(0);
+          c[tx] = a[tx] + b[tx];
+        } /* vector_sum */
+      }.dup;
       cl_int status;
-      cl_mem_flags flags = CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR;
-      cl_mem dA = clCreateBuffer(runtime.context, flags, cl_float.sizeof * A.length, A.ptr, &status);
+      size_t size = code.length;
+      char*[] strs = [code.ptr];
+      auto program = clCreateProgramWithSource(runtime.context, 1, strs.ptr, &size, &status);
+      assert(status == CL_SUCCESS, "this " ~ cl_strerror(status));
+      status = clBuildProgram(program, 1, &runtime.device, "", null, null);
+      if (status != CL_SUCCESS)
+      {
+        char[1024] log;
+        clGetProgramBuildInfo(program, runtime.device, CL_PROGRAM_BUILD_LOG, 1023, log.ptr, null);
+        writeln("CL_PROGRAM_BUILD_LOG:\n", log, "\nEOD");
+      }
+      assert(status == CL_SUCCESS, "this " ~ cl_strerror(status));
+      cl_kernel kernel = clCreateKernel(program, "vector_sum", &status);
+      assert(status == CL_SUCCESS, "this " ~ cl_strerror(status));
+      status = clReleaseProgram(program);
+      assert(status == CL_SUCCESS, "this " ~ cl_strerror(status));
+
+      cl_mem dA = clCreateBuffer(runtime.context, CL_MEM_READ_ONLY, cl_float.sizeof * size, null, &status);
+      assert(status == CL_SUCCESS, "opencl_compute clCreateBuffer A " ~ cl_strerror(status));
+      cl_mem dB = clCreateBuffer(runtime.context, CL_MEM_READ_ONLY, cl_float.sizeof * size, null, &status);
+      assert(status == CL_SUCCESS, "opencl_compute clCreateBuffer B " ~ cl_strerror(status));
+      cl_mem dC = clCreateBuffer(runtime.context, CL_MEM_WRITE_ONLY, cl_float.sizeof * size, null, &status);
+      assert(status == CL_SUCCESS, "opencl_compute clCreateBuffer C " ~ cl_strerror(status));
+      status = clEnqueueWriteBuffer(runtime.queue, dA, CL_TRUE, 0, cl_float.sizeof * size, A.ptr, 0, null, null);
       assert(status == CL_SUCCESS, "opencl_compute " ~ cl_strerror(status));
-      cl_mem dB = clCreateBuffer(runtime.context, flags, cl_float.sizeof * B.length, B.ptr, &status);
-      assert(status == CL_SUCCESS, "opencl_compute " ~ cl_strerror(status));
-      flags = CL_MEM_WRITE_ONLY | CL_MEM_USE_HOST_PTR;
-      cl_mem dC = clCreateBuffer(runtime.context, flags, cl_float.sizeof * C.length, C.ptr, &status);
+      status = clEnqueueWriteBuffer(runtime.queue, dB, CL_TRUE, 0, cl_float.sizeof * size, B.ptr, 0, null, null);
       assert(status == CL_SUCCESS, "opencl_compute " ~ cl_strerror(status));
       status = clSetKernelArg(kernel, 0, cl_mem.sizeof, &dA);
       assert(status == CL_SUCCESS, "opencl_compute " ~ cl_strerror(status));
@@ -149,16 +141,20 @@ class Application {
 
   /++
    +/
-  void clop_compute()
+  void validate()
   {
-    mixin (compile(q{ NDRange(i : 0 .. size) { C[i] = A[i] + B[i]; } }));
+    uint diff = 0;
+    foreach (ii; 0 .. size)
+      if (C[ii] != A[ii] + B[ii])
+        ++diff;
+    if (diff > 0)
+      writeln("DIFFs ", diff);
   }
 
   /++
    +/
   void run()
   {
-    size_t size;
     StopWatch timer;
     TickDuration ticks;
 
@@ -178,7 +174,7 @@ class Application {
     writefln("CLOP   %5.3f [s]", ticks.usecs / 1E6);
     validate();
   }
-}
+} // Application class
 
 int main(string[] args)
 {
@@ -196,7 +192,7 @@ int main(string[] args)
       }
       catch (Exception msg)
       {
-        writeln("BP: ", msg);
+        writeln("algebra: ", msg);
         return -1;
       }
       finally
